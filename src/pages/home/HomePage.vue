06119ffb291778d4950457c5d814bf1b0197108c
nikita.yakushev <nikita.yakushev@g5.com>
2026-06-10 14:47:35 +0300

    Texts

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,32 +1,32 @@
 <script setup>
-  import { computed } from 'vue'
-  import { useQuasar } from 'quasar'
+  /*import { computed } from 'vue'
+  import { useQuasar } from 'quasar'*/
 
   import TitleSection from './components/TitleSection.vue'
   import ProjectSection from './components/ProjectSection.vue'
   import LaunchSection from './components/LaunchSection.vue'
   import AliasesSection from './components/AliasesSection.vue'
   import PasswordSection from './components/PasswordSection.vue'
-  import AboutSection from './components/AboutSection.vue'
+  /*import AboutSection from './components/AboutSection.vue'*/
   import SftpSection from './components/SftpSection.vue'
-  import TestimonialsSection from './components/TestimonialsSection.vue'
+  /*import TestimonialsSection from './components/TestimonialsSection.vue'*/
   import CtaSection from './components/CtaSection.vue'
   import AppVideoDialog from './../blocks/AppVideoDialog.vue'
 
-  const $q = useQuasar()
-  const showAboutSection = computed(() => $q.screen.width >= 1240)
+  /*const $q = useQuasar()
+  const showAboutSection = computed(() => $q.screen.width >= 1240)*/
 </script>
 
 <template>
   <q-page class="fit q-pb-xl">
     <TitleSection />
-    <AboutSection v-if="showAboutSection" />
+<!--    <AboutSection v-if="showAboutSection" />-->
     <ProjectSection />
     <LaunchSection />
     <AliasesSection />
     <PasswordSection />
     <SftpSection />
-    <TestimonialsSection />
+<!--    <TestimonialsSection />-->
     <CtaSection />
   </q-page>
 
